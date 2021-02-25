eadb5ae54dd00a5bed5ba20fc07697649d03422a
npkantelberg <npkantelberg@gmail.com>
2021-02-25 07:59:05 -0600

    got some of the edit functions working and started adding js to hide and show edit functionality.

--- components/Recipe.js
+++ components/Recipe.js
@@ -5,14 +5,20 @@ class Recipe extends React.Component{
     this.props.addToMealPlan(this.props.index);
   }
 
+  startEdit = () => {
+    $(this).parents('.recipe-wrapper').find('.recipe-input').toggleClass('show');
+    console.log($(this).text());
+  }
+
   handleChange = (e) => {
     const updatedRecipe = {
       ...this.props.details,
-      [this.props.details.name]: e.currentTarget.value
+      [e.currentTarget.name]: e.currentTarget.value
     };
     // const name = this.props.details.name;
 
     console.log(this.props.details.instructions); 
+
     console.log(updatedRecipe);
 
     this.props.updateRecipe(this.props.index, updatedRecipe);
@@ -29,13 +35,15 @@ class Recipe extends React.Component{
     return(
       <div className="recipe-wrapper">
         <h2 className="recipe-name">{name}</h2>
+        <input className="recipe-input" type="text" name="name" onChange={this.handleChange} value={name}/>
         <img src={image} alt={name}/>
         <h4>Ingredients</h4>
-        <div className="ingredients-list">{ingredients.map((ingredient) => <input name={name} onChange={this.handleChange} key={ingredient} value={ingredient}></input>)}</div>
+        <div className="ingredients-list">{ingredients.map((ingredient) => <input name="ingredients" onChange={this.handleChange} key={ingredient} value={ingredient}></input>)}</div>
         <h4>Instructions</h4>
-        <div className="instructions"><textarea onChange={this.handleChange} name={name} rows="20" value={instructions}></textarea></div>
+        <div className="instructions"><textarea onChange={this.handleChange} name="instructions" rows="20" value={instructions}></textarea></div>
         <button onClick={this.handleClick}>Add to Meal Plan</button>
         <button onClick={() => {this.props.deleteRecipe(this.props.index)}}>Remove</button>
+        <button onClick={this.startEdit} className="edit-recipe">Edit</button>
       </div>
     )
   }
---
+++ customjs.js
@@ -0,0 +1,4 @@
+// $('.edit-recipe').on('click', function(){
+//   $(this).parents('.recipe-wrapper').find('.recipe-input').toggleClass('show');
+//   console.log($(this).parents('.recipe-wrapper'));
+// });
