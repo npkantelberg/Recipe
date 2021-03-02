6183fb13ef0e785badd98435e83d86c7d3e946a9
npkantelberg <npkantelberg@gmail.com>
2021-03-01 20:34:54 -0600

    Implimented edit and done editing buttons for the edit recipe inputs

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 class Recipe extends React.Component{
+
+  state = { active: false }
+
   handleClick = () => {
     this.props.addToMealPlan(this.props.index);
   }
 
-  startEdit(edit){
-    const theButton = edit.target
-    // edit.parents('.recipe-wrapper').find('.recipe-input').toggleClass('show');
-    console.log(theButton);
+  startEdit(){
+    
+    // const theButton = edit.target
+    // // edit.parents('.recipe-wrapper').find('.recipe-input').toggleClass('show');
+    // console.log(theButton);
+
+    
+      if(this.state.isActive == true){
+        this.setState({
+          isActive: false
+        })
+      } else {
+        this.setState({
+          isActive: true
+        })
+      }
+    // this.setState({
+    //   isActive: !this.state.isActive
+    // });
+    
+    console.log(this.state.isActive);
   }
 
   handleChange = (e) => {
@@ -31,18 +51,32 @@ class Recipe extends React.Component{
     const instructions = this.props.details.instructions;
     const ingredients = this.props.details.ingredients;
 
+    
+
     return(
       <div className="recipe-wrapper">
-        <h2 className="recipe-name">{name}</h2>
-        <input className="recipe-input" type="text" name="name" onChange={this.handleChange} value={name}/>
-        <img src={image} alt={name}/>
+        <h2 readOnly readOnlySwitch={this.state.active && 'active'} className="recipe-read-only recipe-name">{name}</h2>
+        <input className="input-switch recipe-input recipe-input-h2" inputSwitch={this.state.active && 'active'} type="text" name="name" onChange={this.handleChange} value={name}/>
+        <img src={image} alt={name}/>   
         <h4>Ingredients</h4>
-        <div className="ingredients-list">{ingredients.map((ingredient) => <input name="ingredients" onChange={this.handleChange} key={ingredient} value={ingredient}></input>)}</div>
+        
+        <div readOnly readOnlySwitch={this.state.active && 'active'} className="recipe-read-only ingredients-list"><ul>{ingredients.map((ingredient) => <li name="ingredients" onChange={this.
+        handleChange} key={ingredient}>{ingredient}</li>)}</ul></div>
+        <div inputSwitch={this.state.active && 'active'} className="input-switch recipe-input ingredients-list">{ingredients.map((ingredient) => <input name="ingredients" onChange={this.
+        handleChange} key={ingredient} value={ingredient}></input>)}</div>
+        
         <h4>Instructions</h4>
-        <div className="instructions"><textarea onChange={this.handleChange} name="instructions" rows="20" value={instructions}></textarea></div>
+        
+        <div readOnly readOnlySwitch={this.state.active && 'active'} className="recipe-read-only recipe-input instructions"><div  name="instructions">{instructions}</div></div>
+        <div inputSwitch={this.state.active && 'active'} className="input-switch recipe-input instructions"><textarea onChange={this.handleChange} name="instructions" rows="20" value={instructions}></textarea></div>
+        
         <button onClick={this.handleClick}>Add to Meal Plan</button>
+        
         <button onClick={() => {this.props.deleteRecipe(this.props.index)}}>Remove</button>
-        <button onClick={(edit) => {this.startEdit(edit)}} className="edit-recipe">Edit</button>
+        
+        <button inputSwitch={this.state.active && 'active'} onClick={() => this.setState({active: !this.state.active})} className="edit-recipe">Edit</button>
+
+        <button inputSwitch={this.state.active && 'active'} onClick={() => this.setState({active: !this.state.active})} className="input-swtich done-edit-recipe">Done</button>
       </div>
     )
   }
