fd2f522a19ee0c9ad811bdc53b2dffde3fbbf066
npkantelberg <npkantelberg@gmail.com>
2021-03-03 19:54:16 -0600

    working on the image uploader

--- base.js
+++ base.js
@@ -1,16 +1,28 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+// import storage from 'firebase';
+
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBSls4TYY_42hIL7Riph-VscbmNbeEal40",
-  authDomain: "kantelberg-recipes.firebaseapp.com",
-  databaseURL: "https://kantelberg-recipes-default-rtdb.firebaseio.com",
+    apiKey: "AIzaSyBSls4TYY_42hIL7Riph-VscbmNbeEal40",
+    authDomain: "kantelberg-recipes.firebaseapp.com",
+    databaseURL: "https://kantelberg-recipes-default-rtdb.firebaseio.com",
+    projectId: "kantelberg-recipes",
+    storageBucket: "kantelberg-recipes.appspot.com",
+    messagingSenderId: "222582341940",
+    appId: "1:222582341940:web:27c6566bfed4b821076561",
+    measurementId: "G-4VSZL6GG2M"
 });
+
+
+const storageVar = firebase.storage();
+
 
 const base = Rebase.createClass(firebaseApp.database());
 
 // This is a named export
 export { firebaseApp };
+export { storageVar };
 
 // This is a default export
 export default base;
--- components/AddRecipeForm.js
+++ components/AddRecipeForm.js
@@ -5,12 +5,14 @@ class AddRecipeForm extends React.Component{
   imageRef = React.createRef();
   ingredientsRef = React.createRef();
   instructionsRef = React.createRef();
+  imageUploadRef = React.createRef();
 
   addRecipe = (e) => {
     e.preventDefault();
     const recipe ={
       name: this.nameRef.current.value,
       image: this.imageRef.current.value,
+      imageUpload: this.imageUploadRef.current.value,
       ingredients: this.ingredientsRef.current.value.split(","),
       groceryListItems: this.ingredientsRef.current.value.split(","),
       instructions: this.instructionsRef.current.value
@@ -19,16 +21,17 @@ class AddRecipeForm extends React.Component{
     // refresh the form
     e.currentTarget.reset();
   };
-
+  
   render(){
     return(
       <form action="" onSubmit={this.addRecipe}>
         <input name="name" ref={this.nameRef} type="text" placeholder="Name"></input>
         <input name="image" ref={this.imageRef} type="text" placeholder="Image"></input>
+        <input type="file" ref={this.imageUploadRef} /> 
         <textarea name="ingredients" ref={this.ingredientsRef} placeholder="Ingredients"></textarea>
         <textarea name="instructions" ref={this.instructionsRef} placeholder="Instructions"></textarea>
         <button type="submit">Add Recipe</button>
-      </form>
+      </form>      
     )
   }
 }
